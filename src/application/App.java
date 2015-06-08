a742ebe81e61052e145ab4a251e5513c846e921e
shtanaka <egustavo4@gmail.com>
2015-06-08 14:32:53 -0300

    hashJoin almost working

--- src/application/App.java
+++ src/application/App.java
@@ -13,22 +13,34 @@ public class App {
 		// List<Aluno> alunos = AlunoDAO.listAlunos();
 		List<Tupla> turmas = TuplaDAO.listTuplas("turmas.csv");
 		List<Tupla> cursos = TuplaDAO.listTuplas("cursos.csv");
-
+		System.out.println(UtilStaticMethods.parseComparable("22"));
 		// printNestedLoop(turmas, cursos);
-		printHashJoin(turmas, cursos);
+		//printHashJoin(turmas, 1, cursos, 0, "nada");
 
 	}
 
-	private static void printHashJoin(List<Tupla> a, List<Tupla> b) {
-		Bucket[] buckets = generateBuckets(a, 0);
-		for (int i = 0; i < buckets.length; i++) {
+	private static void printHashJoin(List<Tupla> a, int indexA, List<Tupla> b,
+			int indexB, String value) {
 
-			if (buckets[i] != null) {
-				List<Tupla> l = buckets[i].getTuplas();
-				for (Tupla t : l) {
-					System.out.println(t.getColunas()[0]);
+		Bucket[] buckets = generateBuckets(a, indexA);
+		Bucket[] bucketsb = generateBuckets(b, indexB);
+
+		int att = UtilStaticMethods.parseComparable(value);
+		int bkt = (int) Math.floor(att / 2);
+
+		Bucket bucketA = buckets[bkt];
+		Bucket bucketB = bucketsb[bkt];
+		List<Tupla> tuplasA = bucketA.getTuplas();
+		List<Tupla> tuplasB = bucketB.getTuplas();
+
+		for (Tupla t1 : tuplasA) {
+			for (Tupla t2 : tuplasB) {
+				if (t1.getColunas()[indexA].equals(t2.getColunas()[indexB])) {
+					if (value.equals("nada")) {
+						if (t1.getColunas()[indexA].equals(value))
+							System.out.println(t1.toString() + t2.toString());
+					}
 				}
-				System.out.println();
 			}
 		}
 
@@ -66,3 +78,10 @@ public class App {
 	}
 
 }
+
+/*
+ * for (int i = 0; i < buckets.length; i++) {
+ * 
+ * if (buckets[i] != null) { List<Tupla> l = buckets[i].getTuplas(); for (Tupla
+ * t : l) { System.out.println(t.getColunas()[0]); } System.out.println(); } }
+ */
